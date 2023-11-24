d88740cad7549c429bd7cedfea80a4aef89ab893
Uday <iamudayhegde@gmail.com>
2023-11-24 09:44:29 +0530

    Adds Singleton pattern

--- design-patterns/src/main/java/io/github/UdayHE/creational/SingletonMain.java
+++ design-patterns/src/main/java/io/github/UdayHE/creational/SingletonMain.java
@@ -5,7 +5,6 @@ public class SingletonMain {
     public static void main(String[] args) {
         Singleton first = Singleton.INSTANCE;
         Singleton second = Singleton.INSTANCE;
-
         System.out.println("First hashcode:" + first.hashCode());
         System.out.println("Second hashcode:" + second.hashCode());
     }
